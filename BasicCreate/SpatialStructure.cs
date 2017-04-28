890861dd4101963ac33feff3eb262fdeb85cf8f6
Martin Cerny <martin.cerny@xbim.cz>
2017-04-28 10:40:33 +0200

    Comments added.

--- BasicCreate/SpatialStructure.cs
+++ BasicCreate/SpatialStructure.cs
@@ -22,14 +22,17 @@ namespace BasicExamples
         {
             Console.WriteLine(string.Format("{0}{1} [{2}]", GetIndent(level), o.Name, o.GetType().Name));
             
+            //only spatial elements can contain building elements
             var spatialElement = o as IIfcSpatialStructureElement;
             if (spatialElement != null)
             {
+                //using IfcRelContainedInSpatialElement to get contained elements
                 var containedElements = spatialElement.ContainsElements.SelectMany(rel => rel.RelatedElements);
                 foreach (var element in containedElements)
                     Console.WriteLine(string.Format("{0}    ->{1} [{2}]", GetIndent(level), element.Name, element.GetType().Name));
             }
 
+            //using IfcRelAggregares to get spatial decomposition of spatial structure elements
             foreach (var item in o.IsDecomposedBy.SelectMany(r => r.RelatedObjects))
                 PrintHierarchy(item, level +1);
         }
